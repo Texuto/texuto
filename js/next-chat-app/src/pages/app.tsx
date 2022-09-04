51128b35dff170b927c0c8df581b0c7c159013bf
blaze-pvt <talwadkerdinesh@gmail.com>
2022-09-04 18:40:48 +0530

    updated

--- js/next-chat-app/src/pages/app.tsx
+++ js/next-chat-app/src/pages/app.tsx
@@ -1,7 +1,7 @@
 import { startTransition, useEffect, useRef, useState } from "react";
 import { useChannelMessage, useReadChannelState } from "@onehop/react";
 import { getErrorMessage } from "../utils/errors";
-import { ChannelName, ChangeChannel } from "../pages/api/channel";
+import { ChannelName, ChangeChannel } from "./api/channel";
 import { Message, PickWhereValuesAre } from "../utils/types";
 
 // checks current hop channel
